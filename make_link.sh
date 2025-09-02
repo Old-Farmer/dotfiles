e84650de98c2a40a1e434c56a57486e087c6b69b
Charlie <1667482085@qq.com>
2025-09-02 18:08:26 +0800

    commit

--- make_link.sh
+++ make_link.sh
@@ -1,13 +1,16 @@
 #/usr/bin/bash
 
-ln -sr ./.bashrc ~/
+ln -sr ./.bash_aliases ~/
 ln -sr ./.inputrc ~/
 ln -sr ./.tmux.conf ~/
-ln -sr ./.ideavimrc ~/
 ln -sr ./.config/nvim ~/.config/
 ln -sr ./.config/vscode_neovim ~/.config/
-ln -sr ./.config/fcitx5 ~/.config/
-ln -sr ./.config/kitty ~/.config/
 ln -sr ./.config/starship.toml ~/.config/
+ln -sr ./.config/alacritty.toml ~/.config/
+
+ln -sr ./.ideavimrc ~/
+ln -sr ./.config/fcitx5 ~/.config/
 ln -sr ./.config/neovide ~/.config/
+
+ln -sr ./.config/kitty ~/.config/
 ln -sr ./.config/git ~/.config/
