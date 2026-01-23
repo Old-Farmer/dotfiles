0244400216396d69ad5c829d9fc909f7da0a520e
charlie <chaishixin2000@qq.com>
2026-01-23 10:49:12 +0800

    commit

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -124,7 +124,6 @@ vim.pack.add({
     version = vim.version.range("1.*"),
   },
   gh("stevearc/conform.nvim"),
-  gh("folke/lazydev.nvim"), -- Just for lua config dev
   gh("mason-org/mason.nvim"),
   gh("windwp/nvim-autopairs"),
   gh("mfussenegger/nvim-lint"),
@@ -196,6 +195,47 @@ local lsp_config = {
     },
   },
   lua_ls = {
+    on_init = function(client)
+      if client.workspace_folders then
+        local path = client.workspace_folders[1].name
+        if
+          path ~= vim.fn.stdpath("config")
+          and (vim.uv.fs_stat(path .. "/.luarc.json") or vim.uv.fs_stat(path .. "/.luarc.jsonc"))
+        then
+          return
+        end
+      end
+
+      client.config.settings.Lua = vim.tbl_deep_extend("force", client.config.settings.Lua, {
+        runtime = {
+          -- Tell the language server which version of Lua you're using (most
+          -- likely LuaJIT in the case of Neovim)
+          version = "LuaJIT",
+          -- Tell the language server how to find Lua modules same way as Neovim
+          -- (see `:h lua-module-load`)
+          path = {
+            "lua/?.lua",
+            "lua/?/init.lua",
+          },
+        },
+        -- Make the server aware of Neovim runtime files
+        workspace = {
+          checkThirdParty = false,
+          library = {
+            vim.env.VIMRUNTIME,
+            -- Depending on the usage, you might want to add additional paths
+            -- here.
+            -- '${3rd}/luv/library',
+            -- '${3rd}/busted/library',
+          },
+          -- Or pull in all of 'runtimepath'.
+          -- NOTE: this is a lot slower and will cause issues when working on
+          -- your own configuration.
+          -- See https://github.com/neovim/nvim-lspconfig/issues/3189
+          -- library = vim.api.nvim_get_runtime_file('', true),
+        },
+      })
+    end,
     settings = {
       Lua = {
         runtime = { version = "LuaJIT" },
@@ -205,6 +245,15 @@ local lsp_config = {
     },
   },
   neocmake = {},
+  rust_analyzer = {
+    settings = {
+      ["rust-analyzer"] = {
+        diagnostics = {
+          enable = false,
+        },
+      },
+    },
+  },
 }
 local lsp_onattach = {
   ---@diagnostic disable-next-line: unused-local
@@ -230,20 +279,6 @@ for server, config in pairs(lsp_config) do
 end
 vim.lsp.enable(vim.tbl_keys(lsp_config))
 local lsp_group = vim.api.nvim_create_augroup("my.lsp.config", { clear = true })
-vim.api.nvim_create_autocmd("FileType", {
-  group = lsp_group,
-  pattern = "lua",
-  callback = function()
-    require("lazydev").setup({
-      library = {
-        -- See the configuration section for more details
-        -- Load luvit types when the `vim.uv` word is found
-        { path = "${3rd}/luv/library", words = { "vim%.uv" } },
-      },
-    })
-  end,
-  once = true,
-})
 vim.api.nvim_create_autocmd("LspAttach", {
   group = lsp_group,
   callback = function(ev)
@@ -252,7 +287,7 @@ vim.api.nvim_create_autocmd("LspAttach", {
       return
     end
     local client = vim.lsp.get_client_by_id(ev.data.client_id)
-    if not client then
+    if client == nil then
       return
     end
 
@@ -280,10 +315,10 @@ vim.api.nvim_create_autocmd("LspProgress", {
     local token = ev.data.params.token
     if value.kind == "begin" then
       local client = vim.lsp.get_client_by_id(client_id)
-      if not client then
+      if client == nil then
         return
       end
-      if not lsp_progress[client_id] then
+      if lsp_progress[client_id] == nil then
         lsp_progress[client_id] = {}
       end
       local progress = {
@@ -367,6 +402,7 @@ require("blink-cmp").setup({
   },
   keymap = {
     ["<cr>"] = { "select_and_accept", "fallback" },
+    ["<C-k>"] = false, -- Use <c-s>
   },
   sources = {
     -- Not use path because sometimes annoying
